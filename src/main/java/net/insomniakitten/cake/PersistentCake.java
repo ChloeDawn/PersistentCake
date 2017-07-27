1543db44c3e1bc9eb84d0d69e4ff793d5b3d224a
Chloe Dawn <chloe@sapphic.dev>
2017-07-27 19:56:16 +0100

    Fix #1 - Thank you, pau101

--- src/main/java/net/insomniakitten/cake/PersistentCake.java
+++ src/main/java/net/insomniakitten/cake/PersistentCake.java
@@ -1,12 +1,14 @@
 package net.insomniakitten.cake;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockCake;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
-import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
@@ -27,25 +29,35 @@ public class PersistentCake {
 
     @SuppressWarnings("ConstantConditions")
     @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public static void onCakeUpdate(BlockEvent.BreakEvent event) {
+    public static void onBlockBreak(BlockEvent.BreakEvent event) {
         if (event.getWorld().isRemote) return;
         BlockPos pos = event.getPos().up();
         IBlockState state = event.getWorld().getBlockState(pos);
         if (state == null) return;
-        if (state.getBlock().equals(Blocks.CAKE)
-                && state.getValue(BlockCake.BITES).equals(0)) {
-                InventoryHelper.spawnItemStack(event.getWorld(),
-                        pos.getX(), pos.getY(), pos.getZ(),
-                        new ItemStack(Items.CAKE));
+        if (state.getBlock().equals(Blocks.CAKE) && state.getValue(BlockCake.BITES).equals(0)) {
+            Block.spawnAsEntity(event.getWorld(), event.getPos(), new ItemStack(Items.CAKE));
         }
     }
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public static void onCakeBroken(BlockEvent.HarvestDropsEvent event) {
+    public static void onHarvestDrops(BlockEvent.HarvestDropsEvent event) {
         if (event.getWorld().isRemote || event.getState() == null) return;
         if (event.getState().getBlock().equals(Blocks.CAKE)
                 && event.getState().getValue(BlockCake.BITES).equals(0)) {
             event.getDrops().add(new ItemStack(Items.CAKE));
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void onNeighborNotify(BlockEvent.NeighborNotifyEvent event) {
+        if (!event.getNotifiedSides().contains(EnumFacing.UP)) return;
+        World world = event.getWorld();
+        BlockPos pos = event.getPos().up();
+        IBlockState state = world.getBlockState(pos);
+        boolean solidBelow = world.getBlockState(pos.down()).getMaterial().isSolid();
+        if (!solidBelow && state.getBlock() == Blocks.CAKE && state.getValue(BlockCake.BITES).equals(0)) {
+            Block.spawnAsEntity(world, pos, new ItemStack(Items.CAKE));
+            world.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
         }
     }
 
