1947e7a10f24288cee075d86ce4ed1ad64d70b78
InsomniaKitten <admin@designdevils.co.uk>
2017-12-29 23:21:11 +0000

    Suppress registry replacement warnings and add Harvestcraft support

--- src/main/java/net/insomniakitten/cake/PersistentCake.java
+++ src/main/java/net/insomniakitten/cake/PersistentCake.java
@@ -1,10 +1,16 @@
 package net.insomniakitten.cake;
 
 import net.minecraft.block.Block;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+
+import javax.annotation.Nullable;
 
 @Mod(modid = PersistentCake.ID, name = PersistentCake.NAME, version = PersistentCake.VERSION)
 @Mod.EventBusSubscriber(modid = PersistentCake.ID)
@@ -16,7 +22,47 @@ public final class PersistentCake {
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void onBlockRegistry(RegistryEvent.Register<Block> event) {
-        event.getRegistry().register(new BlockPersistentCake());
+        ModContainer minecraft = Loader.instance().getMinecraftModContainer();
+        event.getRegistry().register(withRegistryName(new BlockPersistentCake(), minecraft, "cake"));
+
+        ModContainer harvestcraft = findContainerFor("harvestcraft");
+        if (harvestcraft != null) {
+            event.getRegistry().registerAll(
+                    withRegistryName(new BlockHarvestcraftCake(4), harvestcraft, "carrotcake"),
+                    withRegistryName(new BlockHarvestcraftCake(5), harvestcraft, "cheesecake"),
+                    withRegistryName(new BlockHarvestcraftCake(6), harvestcraft, "cherrycheesecake"),
+                    withRegistryName(new BlockHarvestcraftCake(4), harvestcraft, "chocolatesprinklecake"),
+                    withRegistryName(new BlockHarvestcraftCake(5), harvestcraft, "holidaycake"),
+                    withRegistryName(new BlockHarvestcraftCake(3), harvestcraft, "lamingtoncake"),
+                    withRegistryName(new BlockHarvestcraftCake(5), harvestcraft, "pavlovacake"),
+                    withRegistryName(new BlockHarvestcraftCake(5), harvestcraft, "pineappleupsidedowncake"),
+                    withRegistryName(new BlockHarvestcraftCake(4), harvestcraft, "pumpkincheesecake"),
+                    withRegistryName(new BlockHarvestcraftCake(6), harvestcraft, "redvelvetcake")
+            );
+        }
+    }
+
+    private static <V extends IForgeRegistryEntry<V>> V withRegistryName(V entry, ModContainer container, String name) {
+        if (container == null) throw new IllegalStateException("ModContainer instance cannot be null!");
+        final ResourceLocation id = new ResourceLocation(container.getModId(), name);
+        final ModContainer active = Loader.instance().activeModContainer();
+        try {
+            Loader.instance().setActiveModContainer(container);
+            entry.setRegistryName(id);
+            return entry;
+        } finally {
+            Loader.instance().setActiveModContainer(active);
+        }
+    }
+
+    @Nullable
+    private static ModContainer findContainerFor(String modid) {
+        for (ModContainer container : Loader.instance().getActiveModList()) {
+            if (container != null && modid.equals(container.getModId())) {
+                return container;
+            }
+        }
+        return null;
     }
 
 }
