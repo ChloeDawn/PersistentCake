5a27e9f6865490723702297dab218a9b87ebfbc3
InsomniaKitten <admin@designdevils.co.uk>
2017-12-30 02:38:37 +0000

    Is this what a living nightmare feels like

--- src/main/java/net/insomniakitten/cake/compat/harvestcraft/BlockHarvestcraftCake.java
+++ src/main/java/net/insomniakitten/cake/compat/harvestcraft/BlockHarvestcraftCake.java
@@ -3,22 +3,29 @@ package net.insomniakitten.cake.compat.harvestcraft;
 import net.insomniakitten.cake.BlockPersistentCake;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.stats.StatList;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 import java.util.Random;
 
 public final class BlockHarvestcraftCake extends BlockPersistentCake {
 
     private final int maxBites;
+    private final String itemName;
 
-    protected BlockHarvestcraftCake(int maxBites) {
+    private Item cakeItem = null;
+
+    protected BlockHarvestcraftCake(String item, int maxBites) {
         this.maxBites = Math.min(maxBites, 6);
+        this.itemName = item;
     }
 
     @Override
@@ -33,7 +40,12 @@ public final class BlockHarvestcraftCake extends BlockPersistentCake {
 
     @Override
     public Item getItemDropped(IBlockState state, Random rand, int fortune) {
-        return Item.getItemFromBlock(this);
+        if (cakeItem == null) {
+            ResourceLocation name = new ResourceLocation("harvestcraft", itemName);
+            Item item = ForgeRegistries.ITEMS.getValue(name);
+            cakeItem = item != null ? item : Items.AIR;
+        }
+        return cakeItem;
     }
 
     private boolean eatCake(World world, BlockPos pos, IBlockState state, EntityPlayer player) {
--- src/main/java/net/insomniakitten/cake/compat/harvestcraft/HarvestcraftCompat.java
+++ src/main/java/net/insomniakitten/cake/compat/harvestcraft/HarvestcraftCompat.java
@@ -4,85 +4,39 @@ import net.insomniakitten.cake.PersistentCake;
 import net.insomniakitten.cake.util.RegistryHelper;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod.EventBusSubscriber(modid = PersistentCake.ID)
-@GameRegistry.ObjectHolder("harvestcraft")
 public final class HarvestcraftCompat {
-
-    public static final Block CARROTCAKE = Blocks.AIR;
-    public static final Block CHEESECAKE = Blocks.AIR;
-    public static final Block CHERRYCHEESECAKE = Blocks.AIR;
-    public static final Block CHOCOLATESPRINKLECAKE = Blocks.AIR;
-    public static final Block HOLIDAYCAKE = Blocks.AIR;
-    public static final Block LAMINGTONCAKE = Blocks.AIR;
-    public static final Block PAVLOVACAKE = Blocks.AIR;
-    public static final Block PINEAPPLEUPSIDEDOWNCAKE = Blocks.AIR;
-    public static final Block PUMPKINCHEESECAKE = Blocks.AIR;
-    public static final Block REDVELVETCAKE = Blocks.AIR;
 
     private HarvestcraftCompat() {}
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void onBlockRegistry(RegistryEvent.Register<Block> event) {
-        ModContainer harvestcraft = RegistryHelper.findContainerFor("harvestcraft");
-        if (harvestcraft != null) {
-            event.getRegistry().registerAll(
-                    RegistryHelper.withRegistryName(new BlockHarvestcraftCake(4), harvestcraft, "carrotcake"),
-                    RegistryHelper.withRegistryName(new BlockHarvestcraftCake(5), harvestcraft, "cheesecake"),
-                    RegistryHelper.withRegistryName(new BlockHarvestcraftCake(6), harvestcraft, "cherrycheesecake"),
-                    RegistryHelper.withRegistryName(new BlockHarvestcraftCake(4), harvestcraft, "chocolatesprinklecake"),
-                    RegistryHelper.withRegistryName(new BlockHarvestcraftCake(5), harvestcraft, "holidaycake"),
-                    RegistryHelper.withRegistryName(new BlockHarvestcraftCake(3), harvestcraft, "lamingtoncake"),
-                    RegistryHelper.withRegistryName(new BlockHarvestcraftCake(5), harvestcraft, "pavlovacake"),
-                    RegistryHelper.withRegistryName(new BlockHarvestcraftCake(5), harvestcraft, "pineappleupsidedowncake"),
-                    RegistryHelper.withRegistryName(new BlockHarvestcraftCake(4), harvestcraft, "pumpkincheesecake"),
-                    RegistryHelper.withRegistryName(new BlockHarvestcraftCake(6), harvestcraft, "redvelvetcake")
-            );
-        }
+        registerCake(event, "carrotcake", "carrotcakeitem", 4);
+        registerCake(event, "cheesecake", "cheesecakeitem", 5);
+        registerCake(event, "cherrycheesecake", "cherrycheesecakeitem", 6);
+        registerCake(event, "chocolatesprinklecake", "chocolatesprinklecakeitem", 4);
+        registerCake(event, "holidaycake", "holidaycakeitem", 5);
+        registerCake(event, "lamingtoncake", "lamingtonitem", 3);
+        registerCake(event, "pavlovacake", "pavlovaitem", 5);
+        registerCake(event, "pineappleupsidedowncake", "pineappleupsidedowncakeitem", 5);
+        registerCake(event, "pumpkincheesecake", "pumpkincheesecakeitem", 4);
+        registerCake(event, "redvelvetcake", "redvelvetcakeitem", 6);
     }
 
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public static void onItemRegistry(RegistryEvent.Register<Item> event) {
-        handleItemBlock(event.getRegistry(), CARROTCAKE, "carrotcakeitem");
-        handleItemBlock(event.getRegistry(), CHEESECAKE, "cheesecakeitem");
-        handleItemBlock(event.getRegistry(), CHERRYCHEESECAKE, "cherrycheesecakeitem");
-        handleItemBlock(event.getRegistry(), CHOCOLATESPRINKLECAKE, "chocolatesprinklecakeitem");
-        handleItemBlock(event.getRegistry(), HOLIDAYCAKE, "holidaycakeitem");
-        handleItemBlock(event.getRegistry(), LAMINGTONCAKE, "lamingtonitem");
-        handleItemBlock(event.getRegistry(), PAVLOVACAKE, "pavlovaitem");
-        handleItemBlock(event.getRegistry(), PINEAPPLEUPSIDEDOWNCAKE, "pineappleupsidedowncakeitem");
-        handleItemBlock(event.getRegistry(), PUMPKINCHEESECAKE, "pumpkincheesecakeitem");
-        handleItemBlock(event.getRegistry(), REDVELVETCAKE, "redvelvetcakeitem");
-    }
-
-    private static void handleItemBlock(IForgeRegistry<Item> registry, Block block, String name) {
+    private static void registerCake(RegistryEvent.Register<Block> event, String blockName, String itemName, int maxBites) {
         ModContainer container = RegistryHelper.findContainerFor("harvestcraft");
         CreativeTabs tab = RegistryHelper.findCreativeTabFor("harvestcraft");
         if (container != null && tab != null) {
-            registry.register(RegistryHelper.withRegistryName(
-                    new ItemBlock(block) {
-                        @Override
-                        public String getUnlocalizedName() {
-                            return "tile." + name;
-                        }
-
-                        @Override
-                        public CreativeTabs getCreativeTab() {
-                            return tab;
-                        }
-                    }, container, name)
-            );
+            Block block = new BlockHarvestcraftCake(itemName, maxBites).setUnlocalizedName(blockName).setCreativeTab(tab);
+            event.getRegistry().register(RegistryHelper.withRegistryName(block, container, blockName));
         }
     }
+
 
 }
